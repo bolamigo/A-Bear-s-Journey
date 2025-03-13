380f93bd75b595f56d0f7158da1d8c6800927ac9
pwouik <pwouick@gmail.com>
2025-03-13 01:44:48 +0100

    add misssing assets and fix bear movement

--- Assets/Scripts/BearController.cs
+++ Assets/Scripts/BearController.cs
@@ -1,3 +1,5 @@
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class BearController : MonoBehaviour
@@ -6,53 +8,52 @@ public class BearController : MonoBehaviour
     private Vector3 currentPosition;
     private Vector3 targetPosition;
     [SerializeField] private Camera cam;
-    private CharacterController characterController;
-    private Animator bearAnimator;
     private static readonly int Run = Animator.StringToHash("Run Forward");
     private static readonly int Idle = Animator.StringToHash("Idle");
+    static Vector3 horizontal(Vector3 vec){
+        return new Vector3(vec.x,0.0f,vec.y);
+    }
 
+    // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        bearAnimator = GetComponent<Animator>();
         currentPosition = transform.position;
         targetPosition = currentPosition;
+        transform.rotation = Quaternion.Euler(0,0,0);
     }
-
-    void FixedUpdate()
-    {
-        if (Input.GetMouseButton(0))
-        {
+    void Update(){
+    }
+    // Update is called once per frame
+    void FixedUpdate(){
+        CharacterController characterController = GetComponent<CharacterController>();
+        Animator bearAnimator = GetComponent<Animator>();
+        currentPosition = transform.position;
+        if(Input.GetMouseButton(0)){
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
+
+            if (Physics.Raycast(ray, out RaycastHit hit)) {
                 targetPosition = hit.point;
+                // Do something with the object that was hit by the raycast.
             }
         }
-    }
+        Vector3 horizontal_target = new(targetPosition.x,transform.position.y,targetPosition.z);
 
-    void Update()
-    {
-        currentPosition = transform.position;
-        Vector3 lookDirection = targetPosition - currentPosition;
-        lookDirection.y = 0; // no rotation on y-axis
+        // Does the ray intersect any objects excluding the player layer
+        Vector3 forward = Vector3.Normalize(targetPosition - transform.position);
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit ground_hit))
 
-        if (lookDirection.magnitude > 0.1f)
-        {
-            transform.rotation = Quaternion.LookRotation(lookDirection);
+        { 
+            transform.LookAt(horizontal_target, ground_hit.normal);
         }
 
-        if (Vector3.Distance(currentPosition, targetPosition) > 2)
-        {
+        if(Vector3.Distance(horizontal(currentPosition),horizontal(targetPosition))>2){
             bearAnimator.SetBool(Run, true);
             bearAnimator.SetBool(Idle, false);
-            characterController.Move(Vector3.Normalize(lookDirection) * speed * Time.deltaTime);
+            characterController.SimpleMove(Vector3.Normalize(targetPosition-currentPosition) * speed);
         }
-        else
-        {
+        else{
             bearAnimator.SetBool(Run, false);
             bearAnimator.SetBool(Idle, true);
         }
     }
-
 }
